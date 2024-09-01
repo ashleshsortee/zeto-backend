c400da8512f25cb8688b17cc09ee608868084387
ashleshsortee <sorteeashlesh@gmail.com>
2024-09-01 17:30:40 +0530

    feat: add transfer functionality

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { Router, Request, Response } from "express";
 import { ethers, BigNumberish } from "ethers";
 import { newUser, newUTXO, User, UTXO } from "../lib/utils";
@@ -58,6 +60,8 @@ async function prepareDepositProof(signer: User, output: UTXO) {
     outputSalts: [output.salt],
     outputOwnerPublicKeys,
   };
+
+  console.log("inputObj", inputObj);
 
   const circuit = await loadCircuit("check_hashes_value");
   const { provingKeyFile } = loadProvingKeys("check_hashes_value");
@@ -144,6 +148,21 @@ async function prepareProof(
     encodedProof,
   };
 }
+let sender = {
+  signer: {
+    provider: {},
+    address: "0xD14Ea1ed35DcE48BE3375aA1162F2B2d6A33a465",
+  },
+  ethAddress: "0xD14Ea1ed35DcE48BE3375aA1162F2B2d6A33a465",
+  babyJubPrivateKey:
+    6324256280312473777967681696479815890870889009628290542929508861189431680681n,
+  babyJubPublicKey: [
+    12272621234975364082857678671838395038892879583462041976847058442189389028017n,
+    280747620178470772301510133895636708710150648976680629139476716976969856109n,
+  ],
+  formattedPrivateKey:
+    6796525494593858591117563813831186312580968701825023242962350832043485719797n,
+};
 
 // Example API route
 router.post("/generate-proof", async (req: Request, res: Response) => {
@@ -153,9 +172,16 @@ router.post("/generate-proof", async (req: Request, res: Response) => {
   const privateKey =
     "0x39d4d54b595b40d36ff118b16cadba2e6accf001030cbc457d5c5ce86f74fd4d";
   const signer = new ethers.Wallet(privateKey, provider);
-  const sender = await newUser(signer);
 
-  const utxo = newUTXO(req.body.amount, sender);
+  if (!sender) {
+    console.log("creating new sender...");
+    sender = await newUser(signer);
+  }
+
+  console.log("console sender", sender);
+
+  const utxo = newUTXO(req.body.amount, sender); // 10
+  console.log("console utxo", utxo);
 
   const result = await prepareDepositProof(sender, utxo);
 
@@ -169,29 +195,102 @@ router.post("/generate-proof", async (req: Request, res: Response) => {
   res.send(jsonString);
 });
 
-router.post("/transfer-proof", async (req: Request, res: Response) => {
-  // sender's input UTXOs
-  const sender = await newUser(signer);
+router.get("/transfer-proof", async (req: Request, res: Response) => {
+  try {
+    // sender's input UTXOs
+    const provider = new ethers.JsonRpcProvider(
+      "https://sepolia.infura.io/v3/a17f5301f00345d88ecae1cbd724e4af"
+    );
+    // const privateKey =
+    //   "0x39d4d54b595b40d36ff118b16cadba2e6accf001030cbc457d5c5ce86f74fd4d";
+    // const signer = new ethers.Wallet(privateKey, provider);
+    // const sender = await newUser(signer);
 
-  const utxo1 = newUTXO(10, sender);
-  const utxo2 = newUTXO(20, sender);
+    const utxo1 = {
+      value: "10",
+      hash: 3646291598752793754812933173898622836657192711694616877305564630588289935803n,
+      salt: 20583737005007232339264018572951112929802360873831628502911119219849902591987n,
+    };
 
-  // proposed output UTXOs
-  const recipientPvtKey = "<replace>";
-  const recipientSigner = new ethers.Wallet(recipientPvtKey, provider);
-  const recipient = await newUser(recipientSigner);
-  const _txo3 = newUTXO(25, recipient);
-  const utxo4 = newUTXO(5, sender, _txo3.salt);
+    const utxo2 = {
+      value: "20",
+      hash: 10515388575941427783546410489884679101503369017269496788303891446055934952156n,
+      salt: 4663863106882579818369667553312278345589314397324994849857636474205837176422n,
+    };
 
-  // prepare proof
-  // const result = await prepareProof(
-  //   circuit,
-  //   provingKey,
-  //   signer,
-  //   inputs,
-  //   outputs,
-  //   owners
-  // );
+    // proposed output UTXOs
+    const recipientPvtKey =
+      "0x956cc3175563cab38cf9402334ba198325b3f3eb16335b3f3a627fd3dfaa6306";
+    const recipientSigner = new ethers.Wallet(recipientPvtKey, provider);
+    const recipient = await newUser(recipientSigner);
+
+    const _txo3 = newUTXO(20, recipient);
+    const utxo4 = newUTXO(10, sender, _txo3.salt);
+
+    const result = await doTransfer(
+      sender,
+      [utxo1, utxo2],
+      [_txo3, utxo4],
+      [recipient, sender]
+    );
+    console.log("console result", { ...result, recipient, sender });
+
+    const jsonString = JSON.stringify(
+      { ...result, owners: [recipient, sender] },
+      (_, value) => (typeof value === "bigint" ? value.toString() : value)
+    );
+
+    console.log("console result", result);
+
+    res.setHeader("Content-Type", "application/json");
+    res.send(jsonString);
+  } catch (err) {
+    console.log("console err", err);
+  }
 });
+
+router.get("/decode", async (req, res) => {
+  const errorData =
+    "0x839251272869565690ebe9e5b9d201fff955770e9d203c6d952e932d8c1660eeb2aa7998";
+  const decoded = ethers.toUtf8String(errorData.slice(138));
+  console.log("Revert reason:", decoded);
+
+  res.send(decoded);
+});
+
+async function doTransfer(
+  signer: User,
+  inputs: UTXO[],
+  outputs: UTXO[],
+  owners: User[]
+) {
+  let inputCommitments: [BigNumberish, BigNumberish];
+  let outputCommitments: [BigNumberish, BigNumberish];
+  let outputOwnerAddresses: [AddressLike, AddressLike];
+  let encodedProof: any;
+
+  const circuit = await loadCircuit("anon");
+  const { provingKeyFile: provingKey } = loadProvingKeys("anon");
+
+  const result = await prepareProof(
+    circuit,
+    provingKey,
+    signer,
+    inputs,
+    outputs,
+    owners
+  );
+  console.log("console result1111", result);
+  inputCommitments = result.inputCommitments;
+  outputCommitments = result.outputCommitments;
+  outputOwnerAddresses = owners.map(
+    (owner) => owner.ethAddress || ZeroAddress
+  ) as [AddressLike, AddressLike];
+  encodedProof = result.encodedProof;
+
+  console.log("console encodedProof", result);
+
+  return result;
+}
 
 export default router;
